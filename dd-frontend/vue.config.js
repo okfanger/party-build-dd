471ef48f2ab1a64396b6f630bc0ce9de5e0bdde9
okfanger <lovefyj616@foxmail.com>
2022-06-11 17:04:46 +0800

    feat(project): add axios, vuex, element-ui, vue-router

--- dd-frontend/vue.config.js
+++ dd-frontend/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  runtimeCompiler: true,
+  publicPath: './',
+  transpileDependencies: true,
+  devServer: {
+    port: 4001,
+    proxy: {  //配置跨域
+      '/api': {
+        target: 'http://localhost:8084/',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+        changOrigin: true,  //允许跨域
+      },
+    }
+  },
 })
