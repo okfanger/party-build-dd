2e56b43edc7e014628aab7db8321fc0a43697710
okfanger <lovefyj616@foxmail.com>
2022-06-11 16:27:05 +0800

    backend init

---
+++ dd-backend/src/main/java/app/ddbackend/DdBackendApplication.java
@@ -0,0 +1,13 @@
+package app.ddbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdBackendApplication.class, args);
+    }
+
+}
